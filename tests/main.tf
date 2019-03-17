282067b8f785d96d52e9bbec3d38d7390260b023
SweetOps <vladimir.fess@gmail.com>
2019-03-17 22:51:34 +0200

    fix: add tests

---
+++ tests/main.tf
@@ -0,0 +1,14 @@
+module "my_awesome_bucket" {
+  source    = "../"
+  name      = "bucket"
+  stage     = "production"
+  namespace = "sweetops"
+}
+
+module "my_awesome_bucket_1" {
+  source    = "../"
+  name      = "bucket"
+  stage     = "production"
+  namespace = "sweetops"
+  enabled   = "false"
+}
